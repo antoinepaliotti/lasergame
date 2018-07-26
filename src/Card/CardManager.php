73dc9509911977b879572a205bd0661e7bc46f42
PAILLARES ALEXANDRE <paillares-a@hotmail.fr>
2018-07-26 10:02:41 +0200

    Changements durant absence Alex

--- src/Card/CardManager.php
+++ src/Card/CardManager.php
@@ -25,48 +25,71 @@ class CardManager
 
 
     public function generateCode($centerCode)
-{
-
-    $code = $centerCode . rand(100000, 999999);
-
-    $sum = 0;
-
-    $chars = str_split($code);
-    foreach ($chars as $char) {
-        $sum += $char;
-    }
-
-    $modulo = $sum % 9;
-
-    $finalCode = $code . $modulo;
-
-    $repository = $this->em->getRepository(Card::class);
-
-    $code = $repository->findByCode($finalCode);  /// implémenter findbycode().
-
-    if ($code !== null)
     {
-        $this->generateCode($centerCode);
+        //Génération des 9 premiers chiffres de la carte
+        $cardCode = $this->generateCardCode();
+        $code = $centerCode . $cardCode;
+
+        //Génération du modulo
+        $modulo = $this->checksum($code);
+
+        //Code final
+        $finalCode = $code . $modulo;
+
+        //Vérification de l'unicité du code final
+        if($this->checkCode($finalCode) === true){
+            $this->generateCode($centerCode);
+        }
+
+        return $finalCode;
     }
 
-    return $finalCode;
+    public function generateCardCode(){
+        return rand(100000, 999999);
     }
 
-public function testgenerateCode($centerCode)
-{
+    public function checksum(float $number){
+        $sum = 0;
 
-    $finalCode = $centerCode;
+        $chars = str_split($number);
+        foreach ($chars as $char) {
+            $sum += $char;
+        }
 
-    $repository = $this->em->getRepository(Card::class);
+        $modulo = $sum % 9;
 
-    $code = $repository->findByCode($finalCode);  /// implémenter findbycode().
+        return $modulo;
+    }
 
-    if ($code !== null)
+    public function checkCode(float $code){
+        $exists = false;
+        $repository = $this->em->getRepository(Card::class);
+
+        $code = $repository->findByCode($code);  /// implémenter findbycode().
+
+        //Le code existe en BDD
+        if ($code !== null) {
+            $exists = true;
+            return $exists;
+        }
+
+        return $exists;
+    }
+
+    public function testgenerateCode($centerCode)
     {
-        $finalCode = $this->generateCode('444');
-    }
 
-    return $finalCode;
-}
+        $finalCode = $centerCode;
+
+        $repository = $this->em->getRepository(Card::class);
+
+        $code = $repository->findByCode($finalCode);  /// implémenter findbycode().
+
+        if ($code !== null) {
+            $finalCode = $this->generateCode('444');
+        }
+
+        return $finalCode;
+    }
 
 }
